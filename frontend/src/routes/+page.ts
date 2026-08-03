35edd68f9347b0e6c81990a70fc10a1133b6a415
stairman06 <36215135+stairman06@users.noreply.github.com>
2026-08-02 21:39:13 -0500

    frontend/fix: incorrect session order

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -2,7 +2,10 @@ import { getSessions } from '$lib/api/session';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
+	const sessions = await getSessions();
+	sessions.sort((a, b) => a.date.localeCompare(b.date));
+	sessions.reverse();
 	return {
-		sessions: await getSessions()
+		sessions
 	};
 };
