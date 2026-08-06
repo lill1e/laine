71e29c59052b95d607bd03565c93dc07928f392f
stairman06 <36215135+stairman06@users.noreply.github.com>
2026-08-05 19:44:31 -0500

    frontend/fix: push subnavigation (closes #2)

---
+++ frontend/src/lib/components/core/Subnavigation.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	const { children } = $props();
+</script>
+
+<nav>
+	{@render children()}
+</nav>
+
+<style>
+	nav {
+		width: 100%;
+		height: 45px;
+		background: #fff;
+
+		> :global(a) {
+			border-top: 3px solid transparent;
+			border-bottom: 3px solid #d00;
+			font-size: 14px;
+			width: max-content;
+			height: 100%;
+			display: flex;
+			align-items: center;
+			padding: 10px;
+			color: #000;
+			text-decoration: none;
+		}
+	}
+</style>
